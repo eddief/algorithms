95b02570fd6d469453bb026811e2dde4a677af2d
eddiefigueroa <eddief@outlook.com>
2013-02-28 22:45:07 -0700

    Add mergesort

---
+++ Used/main.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sh collect.sh ExchangeSort e_log.txt e_counts.txt e_times.txt e_avgcounts.txt e_avgtimes.txt
+sh collect.sh InsertionSort i_log.txt i_counts.txt i_times.txt i_avgcounts.txt i_avgtimes.txt
+sh collect.sh MergeSort m_log.txt m_counts.txt m_times.txt m_avgcounts.txt m_avgtimes.txt
--- main.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sh collect.sh ExchangeSort e_log.txt e_counts.txt e_times.txt e_avgcounts.txt e_avgtimes.txt
-
-sh collect.sh InsertionSort i_log.txt i_counts.txt i_times.txt i_avgcounts.txt i_avgtimes.txt
