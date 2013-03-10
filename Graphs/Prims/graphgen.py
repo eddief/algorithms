05c77c7b0bff9c73657ebf59f35569e7f19acbef
eddiefigueroa <eddief@outlook.com>
2013-03-09 19:58:54 -0700

    Add README, restructure folder hierarchy

---
+++ Graphs/Prims/graphgen.py
@@ -0,0 +1,18 @@
+import random
+import sys
+import math
+
+name = sys.argv[1]
+num = sys.argv[2]
+
+f = open(name, 'w')
+for i in range(1, int(num)):
+
+	for k in range(1, int(num)):
+
+		c = 0
+		while c == 0:
+			c = random.choice(range(100))
+		if i != k:
+			s = "" + str(i) + ", " + str(k) + ", " + str(c)
+			f.write(s + "\n")
---
+++ README.md
@@ -0,0 +1,3 @@
+## CP407: Analysis of Algorithms
+
+This repository contains work done for this class. Spring 2013.
