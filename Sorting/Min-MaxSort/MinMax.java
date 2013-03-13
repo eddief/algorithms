9b2359731299f0f3006bfec2220e627f92f5c86e
eddiefigueroa <eddief@outlook.com>
2013-03-13 03:35:05 -0600

    Finish Min-Max Sort, prepare for automation

--- Sorting/Min-MaxSort/MinMax.java
+++ Sorting/Min-MaxSort/MinMax.java
@@ -1,10 +1,32 @@
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MinMax{
 
-	private int[] l;
+	private Integer[] l;
+	private int comps;
 
-	public MinMax(int[] m){
-		this.l = m;	
+	public MinMax(Integer[] m){
+		this.l = m;
 
+		long startTime = System.nanoTime();
+		sort();		
+		long timetaken = System.nanoTime() - startTime;
+
+		System.out.println("Number of Comparisons:" + comps);
+		//System.out.println("Time taken to sort: " + timetaken);
+
+		//writeData(comps, ".txt");
+		writeData(timetaken, "mimx_time.txt");		
+	}
+
+	public void sort(){
 		int begin = 0;
 		int end = l.length-1;
 
@@ -14,6 +36,8 @@ public class MinMax{
 		    int max = end;
 
 		    for(it = begin; it <= end; it++){
+
+		    	comps++;
 
 		        if(l[it] < l[min]){
 		            min = it;
@@ -42,8 +66,7 @@ public class MinMax{
 
 		}
 
-		display();
-
+		//display();
 	}
 
 	public void display(){
@@ -60,9 +83,43 @@ public class MinMax{
 		l[y] = temp;
 	}
 
+    public void writeData(long datum, String filename){
+
+        try{
+            File file = new File(filename);
+
+            if(!file.exists()){
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getName(), true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            String content = "" + datum + "\n";
+            bw.write(content);
+            bw.close();
+            
+        } catch (IOException e){
+                e.printStackTrace();
+        }
+
+    }
+
 
 	public static void main(String[] args) {
-		int[] m = {7, 8, 4, 5, 0, 1, 11, 3};
+
+		String filename = args[0];
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))){
+			String curr;
+			while ((curr = br.readLine()) != null) {
+				temp.add(Integer.parseInt(curr));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Integer[] m = temp.toArray(new Integer[temp.size()]);
 		new MinMax(m);
 	}
 
